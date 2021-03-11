53d52cef36740952cb0e4bee6f0f577c2a2b9d94
sumaiya aktar <sumaiyaaktar3029@gmail.com>
2021-03-11 23:14:20 +0600

    add

--- src/Component/League/League.js
+++ src/Component/League/League.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState }  from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight} from '@fortawesome/free-solid-svg-icons';
-import { Button,Card } from 'react-bootstrap';
-import { Link, useHistory } from 'react-router-dom';
+
+import {useHistory } from 'react-router-dom';
 import './League.css';
 
 
